5d5607adb08d3c088ebc8cf34a5c42900fb3e64e
jin0792 <164024730+jin0792@users.noreply.github.com>
2025-03-04 23:06:41 +0900

    Initial commit

---
+++ src/project/Calculator.java
@@ -0,0 +1,14 @@
+package project;
+
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("첫 번째 숫자를 입력하세요: ");
+        System.out.print("두 번째 숫자를 입력하세요: ");
+
+
+    }
+}
